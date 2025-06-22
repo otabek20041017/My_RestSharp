2dee9178a3c66fadd747160115e46ae5f38757ce
Otabek Uzbeg <otabek.yusupov20041017@gmail.com>
2025-06-22 13:46:16 +0500

    My first API on C# with RestSharp

---
+++ Rest_Sharp_API/Rest-Api.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RestSharp;
+namespace Rest_Sharp_API
+{
+    public class Rest_Api
+    {
+        static void Main(string[] args)
+        {
+            // Create RestClient with API base URL
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+
+            // Create Get request for /posts/1
+            var request = new RestRequest("/posts/1", Method.Get);
+            var request2 = new RestRequest("/comments/1", Method.Get);
+
+            // Execute request and get response
+            var response = client.Execute(request);
+
+            var comments_response = client.Execute(request2);
+            // Checking the status and printing the response
+
+            Console.WriteLine($"\nStatus: {response.StatusCode}");
+            Console.WriteLine($"Content: {response.Content}");
+            Console.WriteLine($"Post-Comments: {comments_response.Content}");
+        }
+    }
+}
