29f657870375f9d49eaf079b6ba5f3664f757fb6
Otabek Uzbeg <otabek.yusupov20041017@gmail.com>
2025-06-23 15:47:53 +0500

    My first POST on RestSharp

---
+++ Rest_Sharp_API/Post_Request.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RestSharp;
+
+namespace Rest_Sharp_API
+{
+    public class Post_Request
+    {
+
+
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("posts", Method.Post);
+
+            request.AddJsonBody(new
+            {
+                userId = 1,
+                title = "Otabek API testing on C#",
+                body = "Hey RestSharp POST!"
+            });
+
+            var response = client.ExecuteAsync(request).Result;
+
+            Console.WriteLine("POST: Response Code - " + response.StatusCode);
+            Console.WriteLine("POST: Response Body - " + response.Content);
+        }
+    }
+}
